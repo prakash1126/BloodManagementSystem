6d566fa40ed1d55c662ffa33f5fb578276b1bc28
prakash1126 <prahlad1126@gmail.com>
2023-01-07 19:54:15 +0545

    register page design

--- client/src/auth/register.js
+++ client/src/auth/register.js
@@ -6,14 +6,35 @@ import '../App.css'
    fullName: Yup.string()
      .min(2, 'Too Short!')
      .max(50, 'Too Long!')
-     .required('Required'),
+     .required('required'),
    age: Yup.string()
-     .min(0, 'Too Short!')
-     .max(3, 'Too Long!')
-     .required('Required'),
+    .min(2, 'Too short!')
+    .max(4, 'Too Long!')
+    .required('required'),
    gender:Yup.string().required("required"),
-   email: Yup.string().email('Invalid email').required('Required'),
-
+   temporaryAddress: Yup.string()
+   .min(2, 'Too short!')
+   .max(50, 'Too Long!')
+   .required('required'),
+   permanentAddress: Yup.string()
+   .min(2, 'Too short!')
+   .max(50, 'Too Long!')
+   .required('required'),
+   email: Yup.string().email('Invalid email').required('required'),
+   phoneNumber: Yup.string()
+   .min(1, 'Too short!')
+   .max(10, 'Too Long!')
+   .required("required"),
+   password: Yup.string()
+   .min(5, "Too Short!")
+   .max(100, "Too Long!")
+   .required("required"),
+ confirmPassword: Yup.string()
+   .min(8, "Too Short!")
+   .max(100, "Too Long!")
+   .required("required")
+   .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  bloodGroup:Yup.string().required("required")
  });
  
  const Register = () => (
@@ -22,9 +43,14 @@ import '../App.css'
      <Formik
        initialValues={{
          fullName: '',
-         age:Number(''),
+         age:Number(),
          gender:'',
+         phoneNumber:Number(),
+         temporaryAddress:'',
+         permanentAddress:'',
          email: '',
+         password:'',
+         bloodGroup:''
        }}
        validationSchema={registerSchema}
        onSubmit={values => {
@@ -39,16 +65,36 @@ import '../App.css'
              <div className="Errors">{errors.fullName}</div>
            ) : null}
            <Field name="age" placeholder="Age" className="Field" />
-           {errors.age && touched.age ? (
-             <div className="Errors">{errors.age}</div>
-           ) : null}
+           {errors.age && touched.age ? (<div className="Errors">{errors.age}</div>) : null}
            <Field  className="Field" as="select" name="gender" placeholder="Gender">
              <option value="male">Male</option>
              <option value="female">Female</option>
              <option value="others">Others</option>
            </Field>
+           {errors.gender && touched.gender ? <div className="Errors">{errors.gender}</div> : null}
            <Field name="email" placeholder="Email" type="email" className="Field" />
            {errors.email && touched.email ? <div className="Errors">{errors.email}</div> : null}
+           <Field name="temporaryAddress" placeholder="Temporary Address" type="text" className="Field" />
+           {errors.temporaryAddress && touched.temporaryAddress ? <div className="Errors">{errors.temporaryAddress}</div> : null}
+           <Field name="permanentAddress" placeholder="Permanent Address" type="text" className="Field" />
+           {errors.permanentAddress && touched.permanentAddress ? <div className="Errors">{errors.permanentAddress}</div> : null}
+           <Field name="phoneNumber" placeholder="Phone Number" className="Field" />
+           {errors.phoneNumber && touched.phoneNumber ? <div className="Errors">{errors.phoneNumber}</div> : null}
+           <Field name="password" placeholder="Password" type="password"className="Field" />
+           {errors.password && touched.password ? <div className="Errors">{errors.password}</div> : null}
+           <Field name="confirmPassword" type="password" placeholder="Confirm Password" className="Field" />
+           {errors.confirmPassword && touched.confirmPassword ? <div className="Errors">{errors.confirmPassword}</div> : null}
+           <Field  className="Field" as="select" name="bloodGroup" placeholder="Blood Group">
+             <option value="A+">A+</option>
+             <option value="A-">A-</option>
+             <option value="B+">B+</option>
+             <option value="B-">B-</option>
+             <option value="O+">O+</option>
+             <option value="O-">O-</option>
+             <option value="AB+">AB+</option>
+             <option value="AB-">AB-</option>
+           </Field>
+           {errors.bloodGroup && touched.bloodGroup ? <div className="Errors">{errors.bloodGroup}</div> : null}
            <button className="btn" type="submit">Submit</button>
          </Form>
        )}

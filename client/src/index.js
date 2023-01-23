d26bb93458191383ac44dc183a7b0ebf84e6a15e
prakash1126 <prahlad1126@gmail.com>
2023-01-23 17:04:53 +0545

    redux is used in project

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,18 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import persistStore from 'redux-persist/es/persistStore'
+import { PersistGate } from "redux-persist/integration/react";
+const persistor=persistStore(store)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { userReducer } from './userReducer'
 
+
+
 export const reducers=combineReducers({
     userDetails:userReducer
 })

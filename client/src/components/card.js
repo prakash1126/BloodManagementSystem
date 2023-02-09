255e43e95828c7a21694df9a248479abdbf1840b
prakash1126 <prahlad1126@gmail.com>
2023-02-09 08:14:54 +0545

    donor page desgined

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,31 +1,52 @@
-import React from 'react'
-import { PhoneOutlined } from '@ant-design/icons';
-import { Button } from '@mui/material';
-import BloodtypeIcon from '@mui/icons-material/Bloodtype';
-import './card.css'
+import React from "react";
+import PhoneIcon from '@mui/icons-material/Phone';
+import { Button, TextField } from "@mui/material";
+import BloodtypeIcon from "@mui/icons-material/Bloodtype";
 
+import "./card.css";
 
-const Card = ({details}) => {
+const Card = ({ details }) => {
+  console.log(details);
+
   return (
     <>
-     <div className='container'>
-      <div className='recieverId'><span>ID:{details.recieverId}</span></div>
-      <div className='details'> 
-      <img src='' alt='image'/>
-      <span><PhoneOutlined/> {details.phoneNumber}</span>
-      <p>{details.detailsNote}</p>
-      <span><BloodtypeIcon/>{details.bloodGroup}</span>
-      <span> Reciever Name: {details.fullName}</span>
+      <div className="container">
+        <div className="recieverId">
+          <span
+            style={{
+              marginLeft: "10px",
+              fontSize: "20px",
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
+            ID:{details.recieverId}
+          </span>
+        </div>
+        <div className="details">
+            <img src="../../public/uploads/logo.png" alt="react logo" style={{ width: '400px', }}/>
+          {/* <img
+            src="../../public/uploads/logo.png"
+            alt="Girl in a jacket"
+            style={{ maxHeight: "50px", maxWidth: "50px" }}
+          /> */}
+          <span style={{display:'flex'}}>
+            <PhoneIcon style={{color: "#5e0431"}}/> {details.phoneNumber}
+          </span>
+          <p>{details.detailsNote}</p>
+          <span style={{ display: "flex" }}>
+            <BloodtypeIcon style={{ color: "#5e0431" }} />
+            {details.bloodGroup}
+          </span>
+          <span> Reciever Name: {details.fullName}</span>
+        </div>
+        <div className="buttons">
+          <Button>Accept</Button>
+          <Button>Reject</Button>
+        </div>
       </div>
-      <div className='buttons'>
-        <Button>Accept</Button>
-        <Button>Reject</Button>
-      </div>
-
-      
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- client/src/donor/donorDashBoard.js
+++ client/src/donor/donorDashBoard.js
@@ -1,28 +1,52 @@
-import React,{useState,useEffect} from 'react'
-import Card from '../components/card'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import Card from "../components/card";
+import axios from "axios";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Donor = () => {
-  const [recieverDetails,setRecieverDetails]=useState([])
-  const fetchDetails=async()=>{
-    const response= await axios.get('http://localhost:5000/reciever')
-    const data=response.data.validDetails
-    setRecieverDetails(data)
-  }
-  useEffect(()=>{
-    fetchDetails()
-  },
-  []
-  )
+  const [recieverDetails, setRecieverDetails] = useState([]);
+  const fetchDetails = async () => {
+    const response = await axios.get("http://localhost:5000/reciever");
+    const data = response.data.validDetails;
+    setRecieverDetails(data);
+  };
+  useEffect(() => {
+    fetchDetails();
+  }, []);
   return (
-    
-       recieverDetails.map((item,id)=>{
-        return(
-          <Card details={item}/>
-        )
-       })
-   
-  )
-}
+    <>
+      <div className="searchBox">
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search......."
+            inputProps={{ "aria-label": "search google maps" }}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </div>
+      {recieverDetails.map((item, id) => {
+        return (
+          <>
+            <Card details={item} />
+          </>
+        );
+      })}
+    </>
+  );
+};
 
-export default Donor
+export default Donor;
